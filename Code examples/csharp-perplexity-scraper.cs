ea07f8696a9dd42445e4f799da1ce1503a85a35b
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2025-09-03 11:16:11 +0300

    Create csharp-perplexity-scraper.cs

---
+++ Code examples/csharp-perplexity-scraper.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OxyApi
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string Username = "USERNAME";
+            const string Password = "PASSWORD";
+
+            var parameters = new
+            {
+                source = "perplexity",
+                prompt = "top 3 smartphones in 2025, compare pricing across US marketplaces",
+                geo_location = "United States",
+                parse = true
+            };
+
+            var client = new HttpClient();
+
+            Uri baseUri = new Uri("https://realtime.oxylabs.io");
+            client.BaseAddress = baseUri;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/v1/queries");
+            requestMessage.Content = JsonContent.Create(parameters);
+
+            var authenticationString = $"{Username}:{Password}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authenticationString));
+            requestMessage.Headers.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+
+            var response = await client.SendAsync(requestMessage);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(contents);
+        }
+    }
+}
