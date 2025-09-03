dd43d792b3c53a0bd30cc9e88e39530d30532d81
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2025-09-03 11:16:37 +0300

    Create java-perplexity-scraper.java

---
+++ Code examples/java-perplexity-scraper.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.concurrent.TimeUnit;
+
+public class Main implements Runnable {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String USERNAME = "USERNAME";
+    public static final String PASSWORD = "PASSWORD";
+
+    public void run() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("source", "perplexity");
+        jsonObject.put("prompt", "top 3 smartphones in 2025, compare pricing across US marketplaces");
+        jsonObject.put("geo_location", "United States");
+        jsonObject.put("parse", true);
+
+        Authenticator authenticator = (route, response) -> {
+            String credential = Credentials.basic(USERNAME, PASSWORD);
+            return response
+                    .request()
+                    .newBuilder()
+                    .header(AUTHORIZATION_HEADER, credential)
+                    .build();
+        };
+
+        var client = new OkHttpClient.Builder()
+                .authenticator(authenticator)
+                .readTimeout(180, TimeUnit.SECONDS)
+                .build();
+
+        var mediaType = MediaType.parse("application/json; charset=utf-8");
+        var body = RequestBody.create(jsonObject.toString(), mediaType);
+        var request = new Request.Builder()
+                .url("https://realtime.oxylabs.io/v1/queries")
+                .post(body)
+                .build();
+
+        try (var response = client.newCall(request).execute()) {
+            if (response.body() != null) {
+                try (var responseBody = response.body()) {
+                    System.out.println(responseBody.string());
+                }
+            }
+        } catch (Exception exception) {
+            System.out.println("Error: " + exception.getMessage());
+        }
+
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Main()).start();
+    }
+}
