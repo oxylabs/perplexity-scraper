6f8e3d88ffc49d235b0bdc3cb1ec782f4c061555
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2026-08-27 12:13:39 +0300

    update curl code

--- Code examples/curl-perplexity-scraper.sh
+++ Code examples/curl-perplexity-scraper.sh
@@ -1,9 +1,10 @@
-curl 'https://realtime.oxylabs.io/v1/queries' \
+curl 'https://data.oxylabs.io/v1/queries' \
 --user 'USERNAME:PASSWORD' \
 -H 'Content-Type: application/json' \
 -d '{
         "source": "perplexity",
-        "prompt": "top 3 smartphones in 2025, compare pricing across US marketplaces",
+        "prompt": "best supplements for better sleep",
         "geo_location": "United States",
-        "parse": true
+        "parse": true,
+        "callback_url": "https://your-server.com/oxylabs-callback"
     }'
